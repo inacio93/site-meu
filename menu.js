2758306a0308e137a8802d04916da2ab0fc1837b
Inácio Santos <manoelinacio09@gmail.com>
2023-10-18 22:38:39 -0300

    trabalho final

---
+++ menu.js
@@ -0,0 +1,21 @@
+//Seleção dos botoes do menu lateral
+var menuItem = document.querySelectorAll('.item-menu')
+
+function selectLink() {
+    menuItem.forEach((item) =>
+        item.classList.remove('ativo')
+    )
+    this.classList.add('ativo')
+}
+
+menuItem.forEach((item) =>
+    item.addEventListener('click', selectLink)
+)
+
+//Funcionamento do botao de expandir
+var btnExp = document.querySelector('#btn-exp')
+var menuSide = document.querySelector('.menu-lateral')
+
+btnExp.addEventListener('click', function () {
+    menuSide.classList.toggle('expandir')
+})
